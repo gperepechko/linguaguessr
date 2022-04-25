3c88cc5d9b415073816dd80ebd463d7893797ca8
MalkOwlet <90442373+MalkOwlet@users.noreply.github.com>
2022-04-25 22:10:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import telebot
-bot = telebot.TeleBot('5294129722:AAGFYo3vV846dMfuLYF_s1VrWmkI-IrZTgA')
-@bot.message_handler(commands=['start'])
+
+bot = telebot.TeleBot('')
+
+@bot.message_handler(commands = ['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Welcome to Linguaguessr! I am thinking of a certain language. Can you guess which one it is?')
+
+@bot.message_handler(content_types = ['text'])
+def send_text(text):
+    if text in 
 
 bot.polling()
