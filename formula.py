dd30cde7ae116067e7779f7ae59cd7af2e3473e6
gperepechko <gperepechko@mail.ru>
2022-04-25 22:30:06 +0300

    Distance formula added

---
+++ formula.py
@@ -0,0 +1,13 @@
+# import pandas as pd
+# df = pd.read_csv('Languages.csv')
+# language = set(df['ascii_name'])
+# lat = set(df['latitude'])
+# lngt = set(df['longitude'])
+# coordinates = dict{language}
+
+x1 = float(input())
+x2 = float(input())
+y1 = float(input())
+y2 = float(input())
+km = ((x1 + x2) ** 2 + (y1 + y2) ** 2) ** (0.5)
+print(km)
--- km.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-df = pd.read_csv('Languages.csv')
-language = set(df['ascii_name'])
-lat = set(df['latitude'])
-lngt = set(df['longitude'])
-coordinates = dict{language}
