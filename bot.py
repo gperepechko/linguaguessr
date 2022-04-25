fadb1c574ee8532a8d6c3d30095d3dec0e7d98f0
MalkOwlet <90442373+MalkOwlet@users.noreply.github.com>
2022-04-25 21:57:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot('5294129722:AAGFYo3vV846dMfuLYF_s1VrWmkI-IrZTgA')
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id, 'Welcome to Linguaguessr! I am thinking of a certain language. Can you guess which one it is?')
